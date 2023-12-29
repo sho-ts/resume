2153d25fa4a512cf0c03d0bea05ba05157edd6d8
sho <sho.webfe@gmail.com>
2023-12-30 05:48:39 +0900

    add: basic認証を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,25 @@
+import BasicAuth from 'nuxt-basic-authentication-module';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: [[BasicAuth, { enabled: true }]],
+  runtimeConfig: {
+    basicAuth: {
+      productionDomains: ['resume.nanolier.art'],
+      pairs: {
+        nanolier: process.env.NUXT_BASIC_AUTH_PASSWORD ?? '%.J$fWEQ6p+X+NVH', // パスワードが万が一設定されなかった際のデフォルト値,
+      },
+    },
+  },
   srcDir: 'src/',
   app: {
     head: {
       htmlAttrs: {
         lang: 'ja',
       },
-    }
+    },
   },
   postcss: {
     plugins: {
