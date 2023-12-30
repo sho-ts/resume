f4f7bbb700ba14d859236e13d1b20f8dbf5fe1d0
sho <sho.webfe@gmail.com>
2023-12-30 09:58:02 +0900

    fix: definePropsでclassを定義していなかったため追加

--- src/components/ui/Box.vue
+++ src/components/ui/Box.vue
@@ -1,3 +1,12 @@
 <template>
   <div class="shadow-[-2px_-2px_4px_4px_rgb(255_255_255)_,2px_2px_4px_4px_rgb(226_232_240)] rounded" :class="class"><slot /></div>
 </template>
+
+<script setup lang="ts">
+defineProps({
+  class: {
+    type: String,
+    default: '',
+  },
+});
+</script>
