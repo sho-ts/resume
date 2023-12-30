40f737a8b0b4ac4c7b7bb1a795c42600de1334f2
sho <sho.webfe@gmail.com>
2023-12-30 20:32:59 +0900

    change: sectionタグで囲むように変更

--- src/components/ui/Section.vue
+++ src/components/ui/Section.vue
@@ -1,8 +1,10 @@
 <template>
-  <Box class="pt-4 pb-6 px-4 md:pt-6 pb-8 md:px-8">
-    <Heading>{{ title }}</Heading>
-    <slot />
-  </Box>
+  <section>
+    <Box class="pt-4 pb-6 px-4 md:pt-6 pb-8 md:px-8">
+      <Heading>{{ title }}</Heading>
+      <slot />
+    </Box>
+  </section>
 </template>
 
 <script setup lang="ts">
