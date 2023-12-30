6a633b3bbc6079c0631d1488e6b5e9440949d1c3
sho <sho.webfe@gmail.com>
2023-12-30 09:01:58 +0900

    fix: iosでbox-shadowが表示されない問題の修正

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -3,7 +3,7 @@
     :type="type"
     :placeholder="props.placeholder"
     :value="props.value"
-    class="bg-slate-100 shadow-[inset_-2px_-2px_1px_1px_rgb(255_255_255)_,inset_2px_2px_1px_1px_rgb(226_232_240)] rounded-full py-2 px-4"
+    class="bg-slate-100 shadow-[inset_-2px_-2px_1px_1px_rgb(255_255_255)_,inset_2px_2px_1px_1px_rgb(226_232_240)] rounded-full py-2 px-4 appearance-none"
   />
 </template>
 
