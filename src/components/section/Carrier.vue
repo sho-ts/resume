dd890785e0625a5be3cde1f7eaa6ac919588e743
sho <sho.webfe@gmail.com>
2023-12-30 10:06:10 +0900

    fix: typeをpasswordに変更

--- src/components/section/Carrier.vue
+++ src/components/section/Carrier.vue
@@ -2,7 +2,7 @@
   <Section title="Carrier">
     <p class="mb-4 text-sm ">Carrierを表示するにはパスワードを入力してください。</p>
     <div class="flex flex-col md:flex-row md:items-center gap-4">
-      <Input :value="value" />
+      <Input type="password" :value="value" />
       <Button class="px-8 py-2 ">表示</Button>
     </div>
   </Section>
