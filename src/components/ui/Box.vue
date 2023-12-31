e3e5b4a8a40f03574b5c706b3e8c4cdc5eec9ce5
sho <sho.webfe@gmail.com>
2024-01-01 03:31:13 +0900

    change: スタイル調整

--- src/components/ui/Box.vue
+++ src/components/ui/Box.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shadow-[-2px_-2px_4px_4px_rgb(255_255_255)_,2px_2px_4px_4px_rgb(226_232_240)] rounded" :class="class"><slot /></div>
+  <div class="shadow-[-5px_-5px_5px_2px_rgb(255_255_255)_,2px_2px_4px_4px_rgb(226_232_240)] rounded" :class="class"><slot /></div>
 </template>
 
 <script setup lang="ts">
