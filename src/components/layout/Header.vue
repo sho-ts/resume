22bb47bb829febaf5a498148e6bfda33de603cc5
sho <sho.webfe@gmail.com>
2024-01-01 04:56:57 +0900

    add: socialリンクを追加

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,13 @@
 <template>
-  <header>
+  <header class="flex justify-between items-center">
     <h1 class="text-3xl">Resume</h1>
+    <ul class="flex items-center gap-2">
+      <li>
+        <a href="https://www.linkedin.com/in/shota-t-a74537246/" target="_blank" rel="noopener noreferrer"><Icon name="mdi:linkedin" size="32px" /></a>
+      </li>
+      <li>
+        <a href="https://twitter.com/sho_ts__" target="_blank" rel="noopener noreferrer"><Icon name="simple-icons:x" size="24px" /></a>
+      </li>
+    </ul>
   </header>
 </template>
