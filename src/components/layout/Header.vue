0478001d0e34a12bfa8daeb7088fe83caa0f0d1d
sho <sho.webfe@gmail.com>
2024-01-15 04:03:06 +0900

    fix: linkedinのリンク修正

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,7 +3,7 @@
     <h1 class="text-3xl">Resume</h1>
     <ul class="flex items-center gap-2">
       <li>
-        <a href="https://www.linkedin.com/in/shota-t-a74537246/" target="_blank" rel="noopener noreferrer"><Icon name="mdi:linkedin" size="32px" /></a>
+        <a href="https://www.linkedin.com/in/shota-tagawa-a74537246" target="_blank" rel="noopener noreferrer"><Icon name="mdi:linkedin" size="32px" /></a>
       </li>
       <li>
         <a href="https://twitter.com/sho_ts__" target="_blank" rel="noopener noreferrer"><Icon name="simple-icons:x" size="24px" /></a>
